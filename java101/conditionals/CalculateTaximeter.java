bfe43837855f6615477715f178af3cb6974ea494
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-01-24 15:57:21 +0300

    calculate taximeter created

--- java101/conditionals/CalculateTaximeter.java
+++ java101/conditionals/CalculateTaximeter.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class CalculateTaximeter {
     /*Write a program that prints the taximeter amount on the screen according to the distance traveled (KM) with Java.
-
     Taximeter costs 2.20 TL per km.
     The minimum amount to be paid is 20 TL. 20 TL will be charged for the fees below 20 TL.
     The taximeter opening fee is 10 TL.
