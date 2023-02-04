464d668a9f66a36e650fe1b81183f2d0ea1f7c2b
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-02-04 15:32:50 +0300

    login created

--- java101/conditionals/CalculateAverage.java
+++ java101/conditionals/CalculateAverage.java
@@ -14,21 +14,27 @@ public class CalculateAverage {
 
         System.out.print("Input Score of Math:");
         math = input.nextInt();
+        math=(math>=0&&math<=100)?math:0;
 
         System.out.print("Input Score of Physics:");
         phys = input.nextInt();
+        phys=(phys>=0&&phys<=100)?phys:0;
 
         System.out.print("Input Score of Chemistry:");
         chem = input.nextInt();
+        chem=(chem>=0&&chem<=100)?chem:0;
 
         System.out.print("Input Score of Turkish:");
         turk = input.nextInt();
+        turk=(turk>=0&&turk<=100)?turk:0;
 
         System.out.print("Input Score of History:");
         hist = input.nextInt();
+        hist=(hist>=0&&hist<=100)?hist:0;
 
         System.out.print("Input Score of Music:");
         mus = input.nextInt();
+        mus=(mus>=0&&mus<=100)?mus:0;
 
         int sum = math + phys + chem + turk + hist + mus;
 
