ffae614415c6760866ee2855df28b3a7f7e6fb0f
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-02-08 14:03:16 +0300

    diamond with stars created

--- java101/loops/TriangleWithStars.java
+++ java101/loops/TriangleWithStars.java
@@ -5,13 +5,22 @@ import java.util.Scanner;
 public class TriangleWithStars {
     public static void main(String[] args) {
 
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Enter number:");
-        int number=input.nextInt();
+        int number = input.nextInt();
 
         for (int i = 0; i < number; i++) {
-            for (int j = 0; j < number-i; j++) {
+            for (int j = 0; j < number - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = number - 2; i >= 0; i--) {
+            for (int j = 0; j < number - i; j++) {
                 System.out.print(" ");
             }
             for (int j = 0; j < 2 * i + 1; j++) {
