78bcd074375c70ad92bcdf74b9fa3ab07631210c
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-02-08 13:58:46 +0300

    triangle with stars created

---
+++ java101/loops/TriangleWithStars.java
@@ -0,0 +1,23 @@
+package java101.loops;
+
+import java.util.Scanner;
+
+public class TriangleWithStars {
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+
+        System.out.println("Enter number:");
+        int number=input.nextInt();
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < number-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
